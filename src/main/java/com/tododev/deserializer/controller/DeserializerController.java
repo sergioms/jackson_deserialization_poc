31198c920d94bd3ac8d601a023e73a1d69a2ffbe
sergio.molina <sergio.molina@gmail.com>
2019-02-24 10:02:33 +0100

    Added json requests. Fixed missing annotation in controller

--- src/main/java/com/tododev/deserializer/controller/DeserializerController.java
+++ src/main/java/com/tododev/deserializer/controller/DeserializerController.java
@@ -13,30 +13,36 @@ public class DeserializerController {
     private static final Logger log = LoggerFactory.getLogger(DeserializerController.class);
 
     @GetMapping
-    public @ResponseBody String ping(){
+    public @ResponseBody
+    String ping() {
         return "hi";
     }
 
     @PostMapping(path = "/bean/secure/files")
-    public @ResponseBody String createListing(@RequestBody com.tododev.deserializer.jackson.bean.FileListSecure filelist){
+    public @ResponseBody
+    String createListing(@RequestBody com.tododev.deserializer.jackson.bean.FileListSecure filelist) {
         return filelist.list();
     }
+
     @PostMapping(path = "/bean/insecure/files")
-    public @ResponseBody String createListing(@RequestBody com.tododev.deserializer.jackson.bean.FileListInsecure filelist){
+    public @ResponseBody
+    String createListing(@RequestBody com.tododev.deserializer.jackson.bean.FileListInsecure filelist) {
         return filelist.list();
     }
+
     @PostMapping(path = "/bean/files")
-    public @ResponseBody String createListing(@RequestBody com.tododev.deserializer.jackson.bean.FileList filelist){
+    public @ResponseBody
+    String createListing(@RequestBody com.tododev.deserializer.jackson.bean.FileList filelist) {
         return filelist.list();
     }
 
     @PostMapping(path = "/class/files")
-    public String createListing(com.tododev.deserializer.jackson.interfaces.subtypeclass.FileList filelist){
+    public String createListing(@RequestBody com.tododev.deserializer.jackson.interfaces.subtypeclass.FileList filelist) {
         return filelist.list();
     }
 
     @PostMapping(path = "/name/files")
-    public String createListing(com.tododev.deserializer.jackson.interfaces.subtypename.FileList filelist){
+    public String createListing(@RequestBody com.tododev.deserializer.jackson.interfaces.subtypename.FileList filelist) {
         return filelist.list();
     }
 }
